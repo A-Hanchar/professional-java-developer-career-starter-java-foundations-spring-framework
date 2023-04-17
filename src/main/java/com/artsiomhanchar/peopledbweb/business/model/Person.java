f3d37e40d99ff1369bead1c99c7a97870b904ccf
Artsiom Hanchar <artsiom.hanchar@gmail.com>
2023-04-17 18:18:17 +0400

    feat: Passed the lesson 243. Introducing Spring Data

--- src/main/java/com/artsiomhanchar/peopledbweb/business/model/Person.java
+++ src/main/java/com/artsiomhanchar/peopledbweb/business/model/Person.java
@@ -1,22 +1,27 @@
 package com.artsiomhanchar.peopledbweb.business.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dob;
     private BigDecimal salary;
-
-
 
 //    public String getFormattedDOB() {
 //        return DateTimeFormatter.ofPattern("MMMM dd, yyyy").format(dob);
--- src/main/java/com/artsiomhanchar/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/artsiomhanchar/peopledbweb/web/controller/PeopleController.java
@@ -1,32 +1,33 @@
 package com.artsiomhanchar.peopledbweb.web.controller;
 
 import com.artsiomhanchar.peopledbweb.business.model.Person;
+import com.artsiomhanchar.peopledbweb.data.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
+import java.util.Iterator;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    private PersonRepository personRepository;
+
+    public PeopleController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @ModelAttribute("people")
+    public Iterable<Person> getPeople() {
+        return personRepository.findAll();
+    }
+
     @GetMapping
-    public String getPeople(Model model) {
-        List<Person> people = List.of(
-                new Person(10L, "Jake", "Snake", LocalDate.of(1950, 1, 1), new BigDecimal("50000")),
-                new Person(20L, "Sarah", "Smith", LocalDate.of(1960, 2, 1), new BigDecimal("60000")),
-                new Person(30L, "Johny", "Jackson", LocalDate.of(1970, 3, 1), new BigDecimal("70000")),
-                new Person(40L, "Bobby", "Kim", LocalDate.of(1980, 4, 1), new BigDecimal("80000")),
-                new Person(50L, "Jerry", "McGuire", LocalDate.of(1997, 4, 1), new BigDecimal("80000")),
-                new Person(60L, "Jane", "Smith", LocalDate.of(1980, 4, 1), new BigDecimal("80000"))
-        );
-
-        model.addAttribute("people",people);
-
+    public String showPeoplePage() {
         return "people";
     }
 }
